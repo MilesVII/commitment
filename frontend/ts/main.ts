beb7a4f985d60a58ad74dcdaa6c8d5c93185fde1
Miles Seventh <milesseventh@gmail.com>
2024-08-28 02:10:28 +0400

    add scaler

--- frontend/ts/main.ts
+++ frontend/ts/main.ts
@@ -42,9 +42,37 @@ function buildGrid() {
 	});
 }
 
+function buildScaler(updateScale: (scale: number) => void) {
+	return buildElement({
+		elementName: "input",
+		attributes: {
+			type: "range",
+			id: "cell-scale",
+			min: "10",
+			max: "700",
+			value: "100"
+		},
+		style: {
+			width: "70%"
+		},
+		events: {
+			input: (e) => {
+				const scale: number = (e.target as any).value / 700;
+				updateScale(scale);
+			}
+		}
+	});
+}
+
 function main() {
 	const canvas = buildGrid();
 	document.querySelector("#canvas-container")?.append(canvas);
+
+	const updateScale = (scale: number) => {
+		canvas.style.setProperty("--cell-size", `${scale * 70}px`);
+	}
+	const scaler = buildScaler(updateScale);
+	document.body.prepend(scaler);
 };
 
 main();
