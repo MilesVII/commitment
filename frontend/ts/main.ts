07a4b736027547013ed4ad7d747774acefd3a888
Miles Seventh <milesseventh@gmail.com>
2024-08-28 20:01:20 +0400

    add palette selector

--- frontend/ts/main.ts
+++ frontend/ts/main.ts
@@ -3,42 +3,50 @@ import { range } from "./utils";
 
 type CellColor = 0 | 1 | 2 | 3 | 4;
 
-function buildCell() {
-	return buildElement({
-		elementName: "div",
-		className: "canvas-grid-cell",
-		state: {
-			color: 0 as CellColor,
-		},
-		events: {
-			pointerdown: (_e, _el, state, update) => {
-				state!.color = 2;
-
-				update!();
-			},
-			pointerenter: (e, _el, state, update) => {
-				if ((e as PointerEvent).buttons <= 0) return;
-
-				state!.color = 4;
-
-				update!();
-			}
-		},
-		update(el, state) {
-			el.style.backgroundColor = `var(--color-gh-${state!.color})`;
-		}
-	});
+const globalState = {
+	selectedColor: 0 as CellColor
 }
 
-function buildGrid() {
-	const gridContents = range(0, 370).map(index =>
-		buildCell()
-	);
+main();
 
+function main() {
+	const palette = buildPalette();
+	const canvas = buildGrid();
+	const updateScale = (scale: number) => {
+		canvas.style.setProperty("--cell-size", `${scale * 70}px`);
+	}
+	const scaler = buildScaler(updateScale);
+	
+	document.body.append(palette);
+	document.body.append(scaler);
+	document.querySelector("#canvas-container")?.append(canvas);
+};
+
+function buildPalette() {
+	function buildColorOption(color: CellColor, onSelect: (color: CellColor, element: HTMLElement) => void) {
+		return buildElement({
+			className: "palette-option",
+			style: {
+				backgroundColor: getCssColor(color)
+			},
+			prefire: (el) => {
+				if (globalState.selectedColor === color)
+					el.classList.add("selected");
+			},
+			events: {
+				click: (_e, el) => onSelect(color, el)
+			}
+		});
+	}
+
+	const options = range(0, 5).map(color => buildColorOption(color as CellColor, (color, element) => {
+		options.forEach(o => o.classList.remove("selected"));
+		element.classList.add("selected");
+		globalState.selectedColor = color;
+	}));
 	return buildElement({
-		elementName: "div",
-		className: "canvas-grid",
-		children: gridContents
+		className: "palette",
+		children: options
 	});
 }
 
@@ -64,15 +72,45 @@ function buildScaler(updateScale: (scale: number) => void) {
 	});
 }
 
-function main() {
-	const canvas = buildGrid();
-	document.querySelector("#canvas-container")?.append(canvas);
+function buildGrid() {
+	const gridContents = range(0, 370).map(index =>
+		buildCell()
+	);
 
-	const updateScale = (scale: number) => {
-		canvas.style.setProperty("--cell-size", `${scale * 70}px`);
-	}
-	const scaler = buildScaler(updateScale);
-	document.body.prepend(scaler);
-};
+	return buildElement({
+		elementName: "div",
+		className: "canvas-grid",
+		children: gridContents
+	});
+}
 
-main();
+function buildCell() {
+	return buildElement({
+		elementName: "div",
+		className: "canvas-grid-cell",
+		state: {
+			color: 0 as CellColor,
+		},
+		events: {
+			pointerdown: (_e, _el, state, update) => {
+				state!.color = globalState.selectedColor;
+
+				update!();
+			},
+			pointerenter: (e, _el, state, update) => {
+				if ((e as PointerEvent).buttons <= 0) return;
+
+				state!.color = globalState.selectedColor;
+
+				update!();
+			}
+		},
+		update(el, state) {
+			el.style.backgroundColor = getCssColor(state!.color);
+		}
+	});
+}
+
+function getCssColor(color: CellColor) {
+	return `var(--color-gh-${color})`;
+}
